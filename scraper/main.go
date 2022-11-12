abb5d9c33fe6cfcc797635962dbff5a80be7ea90
JustTyping1 <aditya.chorghade@outlook.com>
2022-11-12 15:13:06 +0000

    basic scraper

--- scraper/main.go
+++ scraper/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	println("Enter topic : ")
 	var topic string
 	fmt.Scan(&topic)
-	println(topic)
+
+	newScanner := colly.NewCollector()
+	newScanner.OnHTML("span.dt", func(h *colly.HTMLElement) {
+		println(h.ChildText("span[class=dtText]"))
+	})
+	newScanner.Visit("https://www.merriam-webster.com/dictionary/pizza")
 }
