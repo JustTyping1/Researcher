db04fed881f11670b346a66e6fa1bfd3710b31f4
JustTyping1 <aditya.chorghade@outlook.com>
2022-11-12 15:54:38 +0000

    images collection

--- scraper/main.go
+++ scraper/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	println("Enter topic : ")
+	println("Enter a one word topic : ")
 	var topic string
 	fmt.Scan(&topic)
 
@@ -16,4 +16,10 @@ func main() {
 		println(h.ChildText("span[class=dtText]"))
 	})
 	newScanner.Visit("https://www.merriam-webster.com/dictionary/" + topic)
+
+	newScanner1 := colly.NewCollector()
+	newScanner1.OnHTML("div. bRMDJf islir", func(h *colly.HTMLElement) {
+		println(h.ChildText("img[data-deffered=1]"))
+	})
+	newScanner1.Visit("https://www.google.com/search?q=" + topic + "&tbm=isch")
 }
